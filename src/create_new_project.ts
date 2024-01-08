ad2f1897035e674fee57af9a311300feb5889d5a
erelsop <e.r.elsop@protonmail.ch>
2024-01-07 22:50:27 -0500

    Update TypeScript settings and include project templates
    Transitioned the project towards a more efficient TypeScript configuration by updating the 'tsconfig.json' and modifying the imports in the project files. Additionally, incorporated TypeScript and JavaScript project templates into the 'createNewProject' function, providing users a choice during project creation. To comply with the changes, '@types/commander' was also added to the development dependencies.

--- src/create_new_project.ts
+++ src/create_new_project.ts
@@ -1,14 +1,14 @@
 import {execSync} from 'child_process';
 import inquirer from 'inquirer';
-import {checkGhInstalled} from './utilities';
+import {checkGhInstalled} from './utilities.js';
 
 interface InquirerResponse {
   createRepo: boolean;
   repoPublic: boolean;
+  template: 'TypeScript' | 'JavaScript';
 }
 
 async function createNewProject(projectName: string): Promise<void> {
-  
   if (!/^[a-z0-9]+(-[a-z0-9]+)*$/.test(projectName)) {
     console.error(
       'Invalid project name format. Should be hyphenated lowercase.'
@@ -17,8 +17,21 @@ async function createNewProject(projectName: string): Promise<void> {
   }
   
   console.log(`Creating project: ${projectName}`);
+  
+  const {template}: Pick<InquirerResponse, 'template'> = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'template',
+      message: 'Which template would you like to use?',
+      choices: ['TypeScript', 'JavaScript'],
+      default: 'TypeScript'
+    }
+  ]);
+  
   const githubRepo: string =
-    'https://github.com/lit/lit-element-starter-ts.git';
+    template === 'TypeScript'
+      ? 'https://github.com/lit/lit-element-starter-ts.git'
+      : 'https://github.com/lit/lit-element-starter-js.git';
   
   try {
     execSync(`git clone ${githubRepo} ${projectName}`, {
@@ -28,47 +41,47 @@ async function createNewProject(projectName: string): Promise<void> {
     execSync('rm -rf .git', {stdio: 'inherit'});
     execSync('npm install', {stdio: 'inherit'});
     
-    if (checkGhInstalled()) {
-      const {createRepo, repoPublic}: InquirerResponse = await inquirer.prompt<
-        InquirerResponse
-      >([
-        {
-          type: 'confirm',
-          name: 'createRepo',
-          message:
-            'Do you want to create a GitHub repository for this project?',
-          default: false
-        },
-        {
-          type: 'confirm',
-          name: 'repoPublic',
-          message: 'Should the repository be public?',
-          default: true,
-          when: (answers: InquirerResponse) => answers.createRepo
-        }
-      ]);
-      
-      if (createRepo) {
-        const repoVisibility: string = repoPublic ? '--public' : '--private';
-        
-        // Initialize Git repository
-        execSync('git init', {stdio: 'inherit'});
-        
-        // Create GitHub repository and set up remote origin
-        execSync(
-          `gh repo create ${projectName} ${repoVisibility} --source=.`,
-          {stdio: 'inherit'}
-        );
-        
-        // Add, commit, and push files to the remote repository
-        execSync('git add .', {stdio: 'inherit'});
-        execSync('git commit -m "Initial commit"', {
-          stdio: 'inherit'
-        });
-        execSync('git branch -M main', {stdio: 'inherit'});
-        execSync('git push -u origin main', {stdio: 'inherit'});
+    if (!checkGhInstalled()) return;
+    
+    const {createRepo, repoPublic}: InquirerResponse = await inquirer.prompt<
+      InquirerResponse
+    >([
+      {
+        type: 'confirm',
+        name: 'createRepo',
+        message:
+          'Do you want to create a GitHub repository for this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'repoPublic',
+        message: 'Should the repository be public?',
+        default: true,
+        when: (answers: InquirerResponse) => answers.createRepo
       }
-    }
+    ]);
+    
+    if (!createRepo) return;
+    
+    const repoVisibility: string = repoPublic ? '--public' : '--private';
+    
+    // Initialize Git repository
+    execSync('git init', {stdio: 'inherit'});
+    
+    // Create GitHub repository and set up remote origin
+    execSync(
+      `gh repo create ${projectName} ${repoVisibility} --source=.`,
+      {stdio: 'inherit'}
+    );
+    
+    // Add, commit, and push files to the remote repository
+    execSync('git add .', {stdio: 'inherit'});
+    execSync('git commit -m "Initial commit"', {
+      stdio: 'inherit'
+    });
+    execSync('git branch -M main', {stdio: 'inherit'});
+    execSync('git push -u origin main', {stdio: 'inherit'});
   } catch (error: any) {
     if (error && typeof error === 'object') {
       console.error('An error occurred:', error);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
-import { createNewProject } from './create_new_project';
+import {program} from 'commander';
+import {createNewProject} from './create_new_project.js';
 
 program
   .command('new <project-name>')
